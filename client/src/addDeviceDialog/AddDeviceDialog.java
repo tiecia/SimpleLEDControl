4e1194354a1653f50e4f05b9c4b2b4c8e51ca3aa
Tie C <usbtyping@gmail.com>
2019-11-23 20:57:35 -0800

    11/23/2019

--- client/src/addDeviceDialog/AddDeviceDialog.java
+++ client/src/addDeviceDialog/AddDeviceDialog.java
@@ -11,11 +11,14 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import javax.swing.JTextField;
+import javax.swing.ListModel;
+
 import java.awt.Rectangle;
 
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.DefaultListModel;
 import javax.swing.border.TitledBorder;
 
 import com.fazecast.jSerialComm.SerialPort;
@@ -27,25 +30,33 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.JComboBox;
 import javax.swing.JList;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+import java.awt.Color;
+import java.awt.Dimension;
 
 public class AddDeviceDialog extends JDialog {
 	
 	//Actions
-	private final RemoveAction removeAction = new RemoveAction();
+	private final RemoveStripAction removeStripAction = new RemoveStripAction();
 	private final AddStripAction addStripAction = new AddStripAction();
+	private final StripListChanged stripListChangedAction = new StripListChanged();
 	
 	//Components
 	private JTextField deviceNameField;
-	private JPanel stripPanel;
 	private JComboBox<String> portComboBox;
 	private JComboBox<String> deviceComboBox;
-	private ArrayList<StripNode> stripNodes;
+	private DefaultListModel<StripNode> stripNodes;
 	
 	//Attributes
 	private boolean canceled;
 	private LEDPort port;
 	private int maxWindowHeight;
 	private JScrollPane scrollPane;
+	private JPanel contentPanel;
+	private JPanel stripNodeInfoPanel;
+	private JList<StripNode> stripJList;
+	private StripNode currentStripNode;
 	
 
 	
@@ -70,21 +81,22 @@ public class AddDeviceDialog extends JDialog {
 	public AddDeviceDialog() {
 		setResizable(true);
 		maxWindowHeight = (int) (getBounds().getX()-300);
-		stripNodes = new ArrayList<>();
+		stripNodes = new DefaultListModel<>();
 		setModal(true);
-		setBounds(100, 100, 500, 372);
+		setBounds(100, 100, 600, 400);
+		setMinimumSize(new Dimension(600,400));
 		getContentPane().setLayout(new BorderLayout());
 		
 		//CONTENT PANEL
-		JPanel contentPanel = new JPanel();
+		contentPanel = new JPanel();
 		scrollPane = new JScrollPane(contentPanel);
 		getContentPane().add(scrollPane, BorderLayout.CENTER);
-		contentPanel.setLayout(new MigLayout("", "[434px,grow]", "[57.00px][74px][grow]"));
+		contentPanel.setLayout(new MigLayout("", "[][200:200.00:200,grow][300:300,grow]", "[57.00px][grow][]"));
 		
 		//DEVICE PANEL
 		JPanel devicePanel = new JPanel();
 		devicePanel.setBorder(new TitledBorder(null, "Device Information", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		contentPanel.add(devicePanel, "cell 0 0,growx,aligny top");
+		contentPanel.add(devicePanel, "cell 0 0 3 1,growx,aligny top");
 		devicePanel.setLayout(new MigLayout("", "[100,left][96px,grow,left]", "[][][][20px][20px]"));
 		
 		//Device Type Label
@@ -119,16 +131,28 @@ public class AddDeviceDialog extends JDialog {
 		deviceNameField.setColumns(10);
 		devicePanel.add(deviceNameField, "cell 1 4,growx,aligny center");
 		
+		//List ScrollPane
+		JScrollPane listScrollPane = new JScrollPane();
+		contentPanel.add(listScrollPane, "cell 1 1,grow");
 		
-		//STRIP PANEL
-		stripPanel = new JPanel();
-		stripPanel.setBorder(null);
-		contentPanel.add(stripPanel, "cell 0 1,grow");
-		stripPanel.setLayout(new MigLayout("", "[434px,grow]", "[74px]"));
+		//Strip JList
+		stripNodes.addElement(new StripNode());
+		stripJList = new JList<StripNode>(stripNodes);
+		stripJList.addListSelectionListener(stripListChangedAction);
+		stripJList.setSelectedIndex(0);
+		listScrollPane.setViewportView(stripJList);
 		
-		//Add Initial Strip Node (Cannot Be Removed)
-		stripNodes.add(new StripNode(true));
-		stripPanel.add(stripNodes.get(stripNodes.size()-1), "cell 0 0,grow");
+		JButton addStripButton = new JButton("Add Strip");
+		addStripButton.addActionListener(addStripAction);
+		contentPanel.add(addStripButton, "flowx,cell 1 2,growx,aligny center");
+		
+		JButton removeStripButton = new JButton("Remove Strip");
+		removeStripButton.addActionListener(removeStripAction);
+		contentPanel.add(removeStripButton, "cell 1 2,growx");
+		
+		stripNodeInfoPanel = new JPanel();
+		stripNodeInfoPanel.setBackground(Color.RED);
+//		contentPanel.add(stripNodeInfoPanel, "cell 2 1,grow");
 			
 		//BUTTON PANE (OK and Cancel Buttons)
 		JPanel buttonPane = new JPanel();
@@ -142,11 +166,6 @@ public class AddDeviceDialog extends JDialog {
 				ok();
 			}
 		});
-		
-		//Add Strip Button (Located In Content Panel)
-		JButton btnAddStrip = new JButton("Add Strip");
-		buttonPane.add(btnAddStrip);
-		btnAddStrip.addActionListener(addStripAction);
 		okButton.setActionCommand("OK");
 		buttonPane.add(okButton);
 		getRootPane().setDefaultButton(okButton);
@@ -163,14 +182,6 @@ public class AddDeviceDialog extends JDialog {
 			
 		
 		
-	}
-	
-	private void addLEDStrip() {
-		StripNode newStripNode = new StripNode(false);
-		stripNodes.add(newStripNode);
-		stripPanel.add(newStripNode, "cell 0 " + (stripNodes.size()-1) + ",grow");
-		Rectangle bounds = getBounds();
-		setBounds((int)bounds.getX(),(int)bounds.getY(),(int)bounds.getWidth(),(int)bounds.getHeight()+150);
 	}
 	
 	private void ok() {
@@ -201,78 +212,95 @@ public class AddDeviceDialog extends JDialog {
 	
 	public ArrayList<LEDStrip> getStrips() {
 		ArrayList<LEDStrip> strips = new ArrayList<LEDStrip>();
-		for(StripNode node: stripNodes) {
-			strips.add(node.getStrip());
+		StripNode[] nodeArray = new StripNode[stripNodes.getSize()];
+		for(int i = 0; i<stripNodes.size(); i++) {
+			strips.add(stripNodes.get(i).getStrip());
 		}
 		return strips;
 	}
 	
+	private class StripListChanged implements ListSelectionListener {
+		public void valueChanged(ListSelectionEvent e) {
+			System.out.println(e.getSource());
+			StripNode selectedNode = (StripNode) ((JList<StripNode>)e.getSource()).getSelectedValue();
+			System.out.println(selectedNode);
+			if(currentStripNode != null) {
+				contentPanel.remove(currentStripNode);
+			}
+			contentPanel.add(selectedNode, "cell 2 1,growx, aligny top");
+			currentStripNode = selectedNode;
+		}
+	}
+	
 	private class AddStripAction implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
-			StripNode newStripNode = new StripNode(false);
-			stripNodes.add(newStripNode);
-			stripPanel.add(newStripNode, "cell 0 " + (stripNodes.size()-1) + ",grow");
-			Rectangle bounds = getBounds();
-			setBounds((int)bounds.getX(),(int)bounds.getY(),(int)bounds.getWidth(),(int)bounds.getWidth());
-		}
-	}
-	
-	private class RemoveAction implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			System.out.println(((JButton)e.getSource()).getParent());
-			StripNode toRemove = (StripNode) ((JButton)e.getSource()).getParent();
-			stripPanel.remove(toRemove);
-//			setBounds(scrollPane.getViewportBorderBounds());
-			Rectangle bounds = getBounds();
-			setBounds((int)bounds.getX(),(int)bounds.getY(),(int)bounds.getWidth(),(int)scrollPane.getViewportBorderBounds().getHeight());
-			getContentPane().repaint(50L);
-		}
-	}
-	
-	private class StripNode extends JPanel {
-		private JTextField nameTextField;
-		private JTextField pinTextField;
-		private JTextField numTextField;
-		
-		private String[] data;
-		private JButton btnRemove;
+			StripNode newStripNode = new StripNode();
+			stripNodes.addElement(newStripNode);
+			stripJList.setSelectedIndex(stripJList.getModel().getSize()-1);
+			System.out.println(stripNodes);
 
-		/**
-		 * Create the panel.
-		 */
-		public StripNode(boolean first) {
-			setBorder(new TitledBorder(null, "LED Strip", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-			setLayout(new MigLayout("", "[100px,center][grow,center]", "[grow][grow][grow][]"));
-			
-			JLabel lblName = new JLabel("Name");
-			add(lblName, "cell 0 0,alignx center,aligny center");
-			
-			nameTextField = new JTextField();
-			add(nameTextField, "cell 1 0,growx,aligny center");
-			nameTextField.setColumns(10);
-			
-			JLabel lblPin = new JLabel("PIN");
-			add(lblPin, "cell 0 1,alignx center,aligny center");
-			
-			pinTextField = new JTextField();
-			add(pinTextField, "cell 1 1,growx,aligny center");
-			pinTextField.setColumns(10);
-			
-			JLabel lblNumberOfLeds = new JLabel("Number of LEDs");
-			add(lblNumberOfLeds, "cell 0 2,alignx center,growy");
-			
-			numTextField = new JTextField();
-			add(numTextField, "cell 1 2,growx,aligny center");
-			numTextField.setColumns(10);
-			if(!first) {
-				btnRemove = new JButton("Remove");
-				btnRemove.addActionListener(removeAction);
-				add(btnRemove, "cell 1 3,alignx right");
-			}
-		}
-		
-		public LEDStrip getStrip() {
-			return new LEDStrip(nameTextField.getText(), Integer.parseInt(numTextField.getText()), Integer.parseInt(pinTextField.getText()));
 		}
 	}
+	
+	private class RemoveStripAction implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			StripNode toRemove = stripJList.getSelectedValue();
+			DefaultListModel<StripNode> listModel = (DefaultListModel<StripNode>) stripJList.getModel();
+			if(listModel.getElementAt(listModel.size()-1) != toRemove) {
+				stripJList.setSelectedIndex(stripJList.getModel().getSize()-1);
+			} else {
+				stripJList.setSelectedIndex(stripJList.getModel().getSize()-2);
+			}
+			listModel.removeElement(toRemove);
+			System.out.println(toRemove);
+			System.out.println(stripNodes);
+		}
+	}
+	
+//	private class StripNode extends JPanel {
+//		private JTextField nameTextField;
+//		private JTextField pinTextField;
+//		private JTextField numTextField;
+//		
+//		private String[] data;
+//		private JButton btnRemove;
+//
+//		/**
+//		 * Create the panel.
+//		 */
+//		public StripNode(boolean first) {
+//			setBorder(new TitledBorder(null, "LED Strip", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+//			setLayout(new MigLayout("", "[100px,center][grow,center]", "[grow][grow][grow][]"));
+//			
+//			JLabel lblName = new JLabel("Name");
+//			add(lblName, "cell 0 0,alignx center,aligny center");
+//			
+//			nameTextField = new JTextField();
+//			add(nameTextField, "cell 1 0,growx,aligny center");
+//			nameTextField.setColumns(10);
+//			
+//			JLabel lblPin = new JLabel("PIN");
+//			add(lblPin, "cell 0 1,alignx center,aligny center");
+//			
+//			pinTextField = new JTextField();
+//			add(pinTextField, "cell 1 1,growx,aligny center");
+//			pinTextField.setColumns(10);
+//			
+//			JLabel lblNumberOfLeds = new JLabel("Number of LEDs");
+//			add(lblNumberOfLeds, "cell 0 2,alignx center,growy");
+//			
+//			numTextField = new JTextField();
+//			add(numTextField, "cell 1 2,growx,aligny center");
+//			numTextField.setColumns(10);
+//			if(!first) {
+//				btnRemove = new JButton("Remove");
+//				btnRemove.addActionListener(removeAction);
+//				add(btnRemove, "cell 1 3,alignx right");
+//			}
+//		}
+//		
+//		public LEDStrip getStrip() {
+//			return new LEDStrip(nameTextField.getText(), Integer.parseInt(numTextField.getText()), Integer.parseInt(pinTextField.getText()));
+//		}
+//	}
 }
