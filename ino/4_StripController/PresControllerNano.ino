fe9921578c92b62b7ab7ca0cc1f3afb88aa4b74e
tyler.ciapalahazleri <tyler.ciapalahazleri@10.0.243.30>
2019-12-09 08:23:43 -0800

    Update INO

---
+++ ino/4_StripController/PresControllerNano.ino
@@ -0,0 +1,58 @@
+#include <Firmata.h>
+#include <FastLED.h>
+#include <Arduino.h>
+#include <HardwareSerial.h>
+#include <HardwareSerial.cpp>
+#include <EEPROM.h>
+
+
+#define NUM_LEDS 60
+
+
+int stripPin;
+int brightness;
+int red;
+int green;
+int blue;
+
+CRGB leds[4][NUM_LEDS];
+
+void setup()
+{
+    Serial.begin(115200);
+    FastLED.addLeds<NEOPIXEL, 3>(leds[0], NUM_LEDS);
+    FastLED.addLeds<NEOPIXEL, 4>(leds[1], NUM_LEDS);
+    FastLED.addLeds<NEOPIXEL, 5>(leds[2], NUM_LEDS);
+    FastLED.addLeds<NEOPIXEL, 6>(leds[3], NUM_LEDS);
+    FastLED.clear(true);
+    
+    FastLED.setBrightness(100);
+    FastLED.show();
+}
+
+void loop()
+{
+  if(Serial.available() >= 5){
+    stripPin = Serial.read();
+    brightness = Serial.read();
+    red = Serial.read();
+    green = Serial.read();
+    blue = Serial.read();
+
+    printDebug();
+
+    FastLED.setBrightness(brightness);
+    for(int j = 0; j<NUM_LEDS; j++){
+      leds[stripPin-3][j].setRGB(red,green,blue);
+    }
+    FastLED.show();
+  }
+}
+
+void printDebug(){
+    Serial.write(stripPin);
+    Serial.write(brightness);
+    Serial.write(red);
+    Serial.write(green);
+    Serial.write(blue);
+}
